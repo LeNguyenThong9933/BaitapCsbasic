be10caa36debbbba387ae1f1d06bb44cc2bca3c1
ADMIN <ADMIN@PINETREE>
2024-01-06 20:51:56 +0700

    Add project files.

---
+++ Bai1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static bool isPrime(int number) 
+        {
+            if (number <=1) return false;
+            for (int i = 2; i < Math.Sqrt(number); i++) 
+            {
+                if (number % i == 0)
+                return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];        
+            Console.WriteLine("Nhap vao 10 so nguyen: ");
+                for (int i = 0; i < 10; i++) 
+                    {
+                        array[i] = int.Parse(Console.ReadLine());
+                    } 
+            //in ra so nguyen to
+            Console.Write("So nguyen to la: " + " ");
+            foreach (int number in array)
+            {
+                if (isPrime(number)) 
+                {
+                    Console.Write(number +" ");
+                }             
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+            int i;
+            Console.Write("Nhap a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1. Cong");
+            Console.WriteLine("2. Tru");
+            Console.WriteLine("3. Nhan");
+            Console.WriteLine("4. Chia");
+            Console.WriteLine("0. Thoat ra");
+            Console.Write("Nhap phep tinh: ");
+            //i = Convert.ToInt32(Console.ReadLine()); // 1: Tính tổng2.Trừ3.Nhân4.Chia0.Thoát Gia tri khac nhap lai
+            while (!int.TryParse(Console.ReadLine(), out i) || i < 0 || i > 4)
+            {
+                Console.WriteLine("Lua chon khong hop le! Vui long chon lai");
+                Console.Write("Nhap phep tinh: ");
+            }
+            switch (i) 
+            {
+                case 0: // thoat
+                    Console.Write("Bye Bye!");
+                    break;
+                case 1: //cong
+                    Console.WriteLine($"a + b = {a + b}");
+                    break;
+                case 2: // tru
+                    Console.WriteLine($"a - b = {a - b}");
+                    break;
+                case 3: // nhan
+                    Console.WriteLine($"a * b = {a * b}");
+                    break; 
+                case 4: // chia
+                    if (b == 0) Console.WriteLine("Phep tinh khong hop le");
+                    else
+                    {
+                        Console.WriteLine($"a / b = {a / b}");                        
+                    }
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
